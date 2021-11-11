e792a192618062bba76076a9153a373b8e1f5ef8
tangxueqing <tangxueqing@qinglianyun.com>
2021-11-11 14:43:50 +0800

    - 增加个人中心

--- lib/BNBmine.dart
+++ lib/BNBmine.dart
@@ -1,24 +1,106 @@
 //项目业务代码 —— 个人中心
 
 import 'package:flutter/material.dart';
+import 'package:fluttermyapp/BNBmine_mock_data.dart';
 
 class MinePage extends StatelessWidget {
+  static const double MARGIN = 10.0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("个人中心"),
+      body: ListView(
+        children: [
+          _headerGridButton(),
+          _dynamicGridButtons(MineMockData.financeGrids(), "金融理财"),
+          _dynamicGridButtons(MineMockData.serviceGrids(), "生活服务"),
+          _dynamicGridButtons(MineMockData.thirdpartyGrids(), "交通出行"),
+          _dynamicGridButtons(MineMockData.shoppingGrids(), "购物消费"),
+        ],
       ),
     );
   }
 
-  Widget _headerGridButton(){
-    return Container();
+  Widget _headerGridButton() {
+    List<Map<String, String>> buttons = MineMockData.headerGrids();
+    return Container(
+      height: 144,
+      margin: EdgeInsets.fromLTRB(MARGIN, MARGIN, MARGIN, MARGIN / 2),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(6.0),
+          gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [Color(0xFF56AF6D), Color(0xFF56AA6D)])),
+      child: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: buttons
+              .map((item) => _getMenu(item['icon'] ?? "", item['name'] ?? "",
+                  color: Colors.white))
+              .toList(),
+        ),
+      ),
+    );
   }
 
-  Widget _getMenu(IconData iconData,String title){
+  Widget _dynamicGridButtons(List<Map<String, String>> buttons, String title,
+      {int crossAxisCount = 4}) {
     return Container(
+      margin: EdgeInsets.fromLTRB(MARGIN, MARGIN, MARGIN, MARGIN),
+      padding: EdgeInsets.all(MARGIN),
+      decoration: BoxDecoration(
+          boxShadow: [BoxShadow(color: Colors.grey)],
+          borderRadius: BorderRadius.circular(6.0),
+          color: Colors.white),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(title, style: TextStyle(color: Colors.grey[700])),
+          SizedBox(
+            height: 20,
+          ),
+          _gridButtons(buttons, crossAxisCount, textColor: Colors.black),
+        ],
+      ),
+    );
+  }
 
+  GridView _gridButtons(List<Map<String, String>> buttons, int crossAxiscount,
+      {Color textColor = Colors.white}) {
+    double gridSpace = 5.0;
+    return GridView.count(
+      crossAxisCount: crossAxiscount,
+      crossAxisSpacing: gridSpace,
+      mainAxisSpacing: gridSpace,
+      // 设置 shrinkWrap 和 physics 参数：停止 GridView 的滚动，防止与 ListView 冲突
+      shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
+      children: buttons.map((item) {
+        return _getMenu(item['icon'] ?? "", item['name'] ?? "",
+            color: textColor);
+      }).toList(),
+    );
+  }
+
+  Widget _getMenu(String icon, String title, {Color color = Colors.black}) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SizedBox(
+          child: Image.asset(icon),
+          width: 50,
+          height: 50,
+        ),
+        SizedBox(
+          height: 5,
+        ),
+        Text(
+          title,
+          style: TextStyle(fontSize: 14, color: color,height: 1),
+        )
+      ],
     );
   }
 }
--- lib/BNBmine_mock_data.dart
+++ lib/BNBmine_mock_data.dart
@@ -1,43 +1,48 @@
 class MineMockData {
-  static List<Map<String, Object>> financeGrids() {
+  static List<Map<String, String>> financeGrids() {
     return [
-      {'name': '信用卡还款', 'icon': 'images/grid-buttons/grid-1-1.png'},
-      {'name': '借钱', 'icon': 'images/grid-buttons/grid-1-2.png'},
-      {'name': '理财', 'icon': 'images/grid-buttons/grid-1-3.png'},
-      {'name': '保险', 'icon': 'images/grid-buttons/grid-1-4.png'},
+      {'name': '信用卡还款', 'icon': 'images/grid-buttons/ic_collection.png'},
+      {'name': '借钱', 'icon': 'images/grid-buttons/ic_collection.png'},
+      {'name': '理财', 'icon': 'images/grid-buttons/ic_collection.png'},
+      {'name': '保险', 'icon': 'images/grid-buttons/ic_collection.png'},
     ];
   }
-  static List<Map<String,Object>> lifeGrids(){
+  static List<Map<String,String>> serviceGrids(){
     return [
-      {'name': '手机充值', 'icon': 'images/grid-buttons/grid-1-1.png'},
-      {'name': '生活缴费', 'icon': 'images/grid-buttons/grid-1-2.png'},
-      {'name': '充值', 'icon': 'images/grid-buttons/grid-1-3.png'},
-      {'name': '城市服务', 'icon': 'images/grid-buttons/grid-1-4.png'},
-      {'name': '公益', 'icon': 'images/grid-buttons/grid-1-4.png'},
-      {'name': '医疗', 'icon': 'images/grid-buttons/grid-1-4.png'},
-      {'name': '健康码', 'icon': 'images/grid-buttons/grid-1-4.png'},
-    ];
-  }
-
-  static List<Map<String, Object>> transGrids() {
-    return [
-      {'name': '出行服务', 'icon': 'images/grid-buttons/grid-1-1.png'},
-      {'name': '火车票机票', 'icon': 'images/grid-buttons/grid-1-2.png'},
-      {'name': '充值', 'icon': 'images/grid-buttons/grid-1-3.png'},
-      {'name': '城市服务', 'icon': 'images/grid-buttons/grid-1-4.png'},
-      {'name': '公益', 'icon': 'images/grid-buttons/grid-1-4.png'},
-      {'name': '医疗', 'icon': 'images/grid-buttons/grid-1-4.png'},
-      {'name': '健康码', 'icon': 'images/grid-buttons/grid-1-4.png'},
-    ];
-  }
-  static List<Map<String,Object>> shoppingGrids(){
-    return [
-      {'name': '信用卡还款', 'icon': 'images/grid-buttons/grid-1-1.png'},
-      {'name': '借钱', 'icon': 'images/grid-buttons/grid-1-2.png'},
-      {'name': '理财', 'icon': 'images/grid-buttons/grid-1-3.png'},
-      {'name': '保险', 'icon': 'images/grid-buttons/grid-1-4.png'},
+      {'name': '手机充值', 'icon': 'images/grid-buttons/ic_collection.png'},
+      {'name': '生活缴费', 'icon': 'images/grid-buttons/ic_collection.png'},
+      {'name': '充值', 'icon': 'images/grid-buttons/ic_collection.png'},
+      {'name': '城市服务', 'icon': 'images/grid-buttons/ic_collection.png'},
+      {'name': '公益', 'icon': 'images/grid-buttons/ic_collection.png'},
+      {'name': '医疗', 'icon': 'images/grid-buttons/ic_collection.png'},
+      {'name': '健康码', 'icon': 'images/grid-buttons/ic_collection.png'},
     ];
   }
 
+  static List<Map<String, String>> thirdpartyGrids() {
+    return [
+      {'name': '出行服务', 'icon': 'images/grid-buttons/ic_collection.png'},
+      {'name': '火车票机票', 'icon': 'images/grid-buttons/ic_collection.png'},
+      {'name': '充值', 'icon': 'images/grid-buttons/ic_collection.png'},
+      {'name': '城市服务', 'icon': 'images/grid-buttons/ic_collection.png'},
+      {'name': '公益', 'icon': 'images/grid-buttons/ic_collection.png'},
+      {'name': '医疗', 'icon': 'images/grid-buttons/ic_collection.png'},
+      {'name': '健康码', 'icon': 'images/grid-buttons/ic_collection.png'},
+    ];
+  }
+  static List<Map<String,String>> shoppingGrids(){
+    return [
+      {'name': '信用卡还款', 'icon': 'images/grid-buttons/ic_collection.png'},
+      {'name': '借钱', 'icon': 'images/grid-buttons/ic_collection.png'},
+      {'name': '理财', 'icon': 'images/grid-buttons/ic_collection.png'},
+      {'name': '保险', 'icon': 'images/grid-buttons/ic_collection.png'},
+    ];
+  }
+  static List<Map<String,String>> headerGrids(){
+    return[
+      {'name':'收付款',"icon":'images/grid-buttons/ic_collection.png'},
+      {'name':'钱包',"icon":'images/grid-buttons/ic_collection.png'},
+    ];
+  }
 
 }

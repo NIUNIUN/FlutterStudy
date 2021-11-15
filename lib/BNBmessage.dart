faaf87b99775f1a42003f4f9e9c2a4bba07ff785
evie <15675478641@163.com>
2021-11-15 08:08:47 +0800

    - 学习多选底部弹框。

--- lib/BNBmessage.dart
+++ lib/BNBmessage.dart
@@ -1,5 +1,6 @@
 // ignore: file_names
 // 项目业务代码 —— 消息
+
 import 'package:flutter/material.dart';
 
 class MessagePage extends StatelessWidget {
@@ -23,11 +24,18 @@ class MessagePage extends StatelessWidget {
               height: 10,
             ),
             TextButton(
-                onPressed: () {
-                  _showCustomModalBottomSheet(
-                      context,  _basicData());
+                onPressed: () async {
+                  _showCustomModalBottomSheet(context, _basicData());
                 },
                 child: Text('自定义底部弹框')),
+            SizedBox(
+              height: 10,
+            ),
+            TextButton(
+                onPressed: () async {
+                  _showMultiChoiceModalButtomSheet(context, _basicData());
+                },
+                child: Text("多选底部弹窗")),
           ],
         ),
       ),
@@ -119,6 +127,98 @@ class MessagePage extends StatelessWidget {
         );
       },
     );
+  }
+
+  Widget _getModalSheetHeaderWithConfirm(String title,
+      {onCancel: Function, onConfirm: Function}) {
+    return SizedBox(
+      height: 50,
+      child: Row(
+        children: [
+          IconButton(
+            onPressed: onCancel,
+            icon: Icon(Icons.close),
+          ),
+          Expanded(
+            child: Center(
+              child: Text(
+                title,
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16.0),
+              ),
+            ),
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.check,
+              color: Colors.green,
+            ),
+            onPressed: () {
+              onConfirm();
+            },
+          )
+        ],
+      ),
+    );
+  }
+
+  Future<List<int>?> _showMultiChoiceModalButtomSheet(
+      BuildContext context, List<String> options) async {
+    Set<int> selected = Set<int>();
+    return showModalBottomSheet<List<int>>(
+        backgroundColor: Colors.transparent,
+        context: context,
+        isScrollControlled: true,
+        builder: (BuildContext context) {
+          return StatefulBuilder(builder: (context1, setState) {
+            return Container(
+              clipBehavior: Clip.antiAlias,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: const Radius.circular(20.0),
+                  topRight: const Radius.circular(20.0),
+                ),
+              ),
+              height: MediaQuery.of(context).size.height / 2.0,
+              child: Column(
+                children: [
+                  _getModalSheetHeaderWithConfirm('多选底部弹窗', onCancel: () {
+                    Navigator.of(context).pop();
+                  }, onConfirm: () {
+                    Navigator.of(context).pop(selected.toList());
+                  }),
+                  Divider(
+                    height: 1.0,
+                  ),
+                  Expanded(
+                      child: ListView.builder(
+                    itemBuilder: (BuildContext context, int index) {
+                      return ListTile(
+                        trailing: Icon(
+                          selected.contains(index)
+                              ? Icons.check_box
+                              : Icons.check_box_outline_blank,
+                          color: Theme.of(context).primaryColor,
+                        ),
+                        title: Text(options[index]),
+                        onTap: () {
+                          setState(() {
+                            if (selected.contains(index)) {
+                              selected.remove(index);
+                            } else {
+                              selected.add(index);
+                            }
+                          });
+                        },
+                      );
+                    },
+                    itemCount: options.length,
+                  )),
+                ],
+              ),
+            );
+          });
+        });
   }
 
 /** 底部弹出框  bottom_sheet.dart 中 定义
